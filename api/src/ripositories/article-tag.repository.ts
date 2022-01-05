d57ef6e7bb57c7290a23bddce75409b3668eb15a
RyosukeSomeya <rx.0.0618@gmail.com>
2022-01-06 02:37:42 +0900

    各repositiry.tsファイル修正

--- api/src/ripositories/article-tag.repository.ts
+++ api/src/ripositories/article-tag.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ArticleTag } from 'src/database/entities/article-tag.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @Injectable()
-@EntityRepository()
-export class ArticleTagRepository {}
+@EntityRepository(ArticleTag)
+export class ArticleTagRepository extends Repository<ArticleTag> {}
